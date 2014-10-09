816171f20744a114fb4cb27ea1a2489ab489ee1a
y.lastapis <y.lastapis@gmail.com>
2014-10-09 16:26:16 +0200

    Skin header

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          // target.css file: source.less file
+          "web/css/styles.css": "src/BlagnacVolley/WebsiteBundle/Resources/public/less/styles.less",
+          "web/css/bootstrap.css": "bower_components/bootstrap/less/bootstrap.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['src/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['watch']);
+
+};
