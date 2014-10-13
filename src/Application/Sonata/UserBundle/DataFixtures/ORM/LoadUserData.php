e4b5aefe959a179285504df656a97d26b82bd5dd
Thomas Lesne <thomas.lesne@eyefinity.com>
2014-10-13 17:15:46 +0200

    Added fixtures bundles and initialized user fixture with admin account

---
+++ src/Application/Sonata/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Application\Sonata\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Application\Sonata\UserBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPlainPassword('admin');
+        $userAdmin->setEmail('admin@admin.com');
+        $userAdmin->setSuperAdmin(true);
+        $userAdmin->setEnabled(true);
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+} 
